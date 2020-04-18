63e70aed8ba9ad224967f4ee98392cf90a26d92a
vovayrovsky <vova@txlib.ru>
2020-04-18 19:24:00 +0300

    Create MakeGraph.py

---
+++ MakeGraph.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tk
+import matplotlib.pyplot as plt
+
+import sys
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+def MakeGraph (X, Y, fname):
+	plt.figure (figsize = (12,12))
+	plt.subplot()
+	plt.bar (X, Y, width = 0.001)
+	plt.savefig (fname)
+	print 'Generated ' + fname
+	return
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+if (len(sys.argv) < 2):
+	print 'No input file'
+	exit()
+	
+
+print 'started'
+
+X,Y = np.genfromtxt(sys.argv[1], unpack=True, skip_footer=1)
+MakeGraph (X, Y, sys.argv[1] + '.png')
